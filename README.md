9ee83f82080a6c0db7be1a1bf48d84cd3ba00205
ruslooob <rm952576@gmail.com>
2024-07-07 14:11:35 +0300

    Create caption on top of generated video

---
+++ README.md
@@ -0,0 +1,2 @@
+скачать ffmpeg, добавить в path
+скачать imagemagick, добавить в переменную среды IMAGEMAGICK_BINARY
--- main.py
+++ main.py
@@ -1,13 +1,18 @@
+from pprint import pprint
 from typing import Literal
 
+import os
 from moviepy.editor import ImageClip, AudioFileClip, CompositeVideoClip
 from moviepy.video.VideoClip import TextClip
+
+IMAGEMAGICK_BINARY = os.getenv('IMAGEMAGICK_BINARY')
 
 
 def create_video_with_image(video_type: Literal['plain', 'short'],
                             image_path: str,
                             audio_path: str,
                             output_path: str,
+                            text: str,
                             duration: int = 10):
     # Load the static image
     image_clip = ImageClip(image_path).set_duration(duration)
@@ -29,8 +34,21 @@ def create_video_with_image(video_type: Literal['plain', 'short'],
     # Set the audio to the image clip
     video_clip = image_clip.set_audio(audio_clip)
 
+    # Create a text clip
+    text_clip = TextClip(text, size=(900, 0), fontsize=60, color='white', font='./fonts/OpenSans-Bold.ttf')
+
+    # Set the duration of the text clip to match the image duration
+    text_clip = text_clip.set_duration(duration)
+
+    # Position the text at the top of the video
+    padding_top = 75
+    text_clip = text_clip.set_position(('center', padding_top))
+
+    # Composite the text clip on the image clip
+    video_with_caption = CompositeVideoClip([video_clip, text_clip])
+
     # Save the final video with proper codec settings
-    video_clip.write_videofile(output_path, codec='libx264', audio_codec='aac', fps=24)
+    video_with_caption.write_videofile(output_path, codec='libx264', audio_codec='aac', fps=24)
 
 
 # Example usage:
@@ -38,4 +56,4 @@ image_path = 'photo.jpg'
 audio_path = 'Hər_kəs_dilinə_hakim_olsa.mp3'
 output_path = 'output_video.mp4'
 
-create_video_with_image('plain', image_path, audio_path, output_path)
+create_video_with_image('plain', image_path, audio_path, output_path, 'Min bir hikmətli söz')
